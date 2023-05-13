3fd57df9cb0c2dc2c95e9a49b8737f147bd619c0
Valarray <istamovvalerij@gmail.com>
2023-05-13 13:52:48 +0300

    Center categories and convert to sass

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -25,72 +25,59 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+.categories
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-wrap: wrap
+  padding: 50px 0
+  border-top: 4px solid var(--seagreen)
 
-.categories {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 50px 0;
-  border-top: 4px solid var(--seagreen);
-}
+  div
+    width: 300px
+    height: 300px
+    overflow: hidden
+    margin-right: 30px
+    border-radius: 10px
+    cursor: pointer
+    position: relative
+    display: flex
+    align-items: center
+    justify-content: center
 
-.categories div {
-  width: 300px;
-  height: 300px;
-  overflow: hidden;
-  margin-right: 30px;
-  border-radius: 10px;
+    &::before
+      content: ''
+      display: block
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      position: absolute
+      background: rgba(128, 128, 128, 0.5)
+      opacity: 100%
+      transition: opacity 0.2s ease-in-out
 
-  cursor: pointer;
-  position: relative;
+    &:hover::before
+      opacity: 0
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+.on-sale, .on-order, .sold
+  background-size: cover
 
-.categories div::before {
-  content: '';
-  display: block;
+.on-sale
+  background-image: url("@/assets/meadow.jpg")
 
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background: rgba(128, 128, 128, 0.5);
+.on-order
+  background-image: url("@/assets/seedlings.jpg")
 
-  opacity: 100%;
-  transition: opacity 0.2s ease-in-out;
-}
+.sold
+  background-image: url("@/assets/mountains.jpg")
 
-.categories div:hover::before{
-  opacity: 0;
-}
+h3
+  z-index: 2
+  color: white
+  font-size: 25px
+  font-weight: 300
+  text-shadow: 0 0 2px #000000
 
-.on-sale,
-.on-order,
-.sold {
-  background-size: cover;
-}
-
-.on-sale{
-  background-image: url("@/assets/meadow.jpg");
-}
-
-.on-order {
-  background-image: url("@/assets/seedlings.jpg");
-}
-
-.sold {
-  background-image: url("@/assets/mountains.jpg");
-}
-
-h3 {
-  z-index: 2;
-  color: white;
-  font-size: 25px;
-  font-weight: 300;
-  text-shadow: 0 0 2px #000000;
-}
 </style>
