425def964bd21f42c37f1f6e4987fc75b4338197
Valarray <istamovvalerij@gmail.com>
2023-05-13 13:49:40 +0300

    Paintings catalog layout

--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,13 +1,24 @@
 <template>
   <h2>Все работы</h2>
-  <div class="all-paintings">
-  <painting-card
-      v-for="(painting, index) in paintings"
-      :key="index"
-      :painting="painting"
-      @click="router.push('/paintingpage' + '/' + painting.title)"
-  >
-  </painting-card>
+  <div class="paint-container">
+    <div v-for="_ in 10" class="paint-block">
+      <div class="paint-info-part">
+        <div class="paint-name">
+          Кин дза дза
+        </div>
+        <div class="paint-description">
+          Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать
+          несколько абзацев более менее осмысленного текста рыбы на русском языке,
+          а начинающему оратору отточить навык публичных выступлений в домашних условиях.
+        </div>
+        <div class="paint-price">
+          12 000 руб
+        </div>
+      </div>
+      <div class="paint-image-part">
+        <img class="paint-image" src="@/assets/test.jpeg" alt="kin-dza-dza"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,19 +29,66 @@ export default {
 </script>
 
 <script setup>
-import PaintingCard       from "@/components/painting/PaintingCard.vue";
-import { paintingsMocks } from "@/mocks/paintings.js";
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-const paintings = ref(paintingsMocks);
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+.paint-container
+  gap: 40px
+  display: flex
+  flex-direction: column
+  background-color: #F6F6F6
 
-.all-paintings {
-  display: flex;
-  flex-wrap: wrap;
-}
+  .paint-block
+    padding: 50px 0
+    display: flex
+
+    &:nth-child(even)
+      flex-direction: row-reverse
+
+      .paint-image-part
+        .paint-image
+          transform: rotate(-3deg)
+
+    .paint-info-part
+      width: 50%
+
+      padding: 0 10%
+      box-sizing: border-box
+
+      text-align: center
+
+      gap: 20px
+      display: flex
+      align-items: center
+      flex-direction: column
+      justify-content: center
+
+      .paint-name
+        color: #461D1D
+        font-size: 48px
+        font-weight: bold
+
+      .paint-description
+        color: #686868
+        font-size: 14px
+        font-weight: bold
+
+      .paint-price
+        color: #373737
+        font-size: 16px
+        font-weight: bold
+
+    .paint-image-part
+      width: 50%
+
+      display: flex
+      align-items: center
+      justify-content: center
+
+      .paint-image
+        height: 400px
+        width: auto
+        box-shadow: -10px 10px 0px 0px rgba(34, 60, 80, 0.2)
+
+        transform: rotate(3deg)
 </style>
