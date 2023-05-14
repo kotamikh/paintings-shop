7f8e41a852b0def70dcde8457e178d8ae78ca56a
Valarray <istamovvalerij@gmail.com>
2023-05-14 13:27:03 +0300

    TheInput and TheTextArea valid states

--- src/components/common/TheInput.vue
+++ src/components/common/TheInput.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="input-container">
-    <label>{{ label }}</label>
-    <input type="text" v-model="modelValue"/>
+  <div
+      class="input-container"
+  >
+    <label>
+      {{ label }}
+    </label>
+    <input
+        type="text"
+        v-model="modelValue"
+    />
+    <div
+        v-if="!valid"
+        class="warning"
+    ></div>
+    <div
+        v-if="!valid"
+        class="hint"
+    >
+      {{ hintText }}
+    </div>
   </div>
 </template>
 
@@ -12,7 +29,7 @@ export default {
 </script>
 
 <script setup>
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 
 const emit = defineEmits(['update:modelValue']);
 
@@ -34,12 +51,41 @@ const modelValue = computed({
     emit('update:modelValue', value)
   }
 })
+
+const hintText = ref('Данное поле обязательно для заполнения');
+const valid = ref(true);
+
+watch(modelValue, newValue => {
+  valid.value = !!newValue;
+}, { immediate: true })
+
+defineExpose({ valid })
 </script>
+
 
 <style scoped lang="sass">
 .input-container
+  position: relative
+
   display: flex
   flex-direction: column
+
+  .warning
+    position: absolute
+    right: 0
+
+    height: 20px
+    width: 20px
+    background-color: #c56f6f
+    border-radius: 50%
+
+    display: flex
+    align-items: center
+    justify-content: center
+
+    &::after
+      content: '!'
+      color: white
 
   label
     color: gray
@@ -54,4 +100,10 @@ const modelValue = computed({
     border-radius: 10px
     border: 1px solid rgba(128, 128, 128, 0.54)
     box-shadow: 0 0 2px 1px rgba(34, 60, 80, 0.2) inset
+
+  .hint
+    font-size: 10px
+    padding-left: 10px
+    padding-top: 5px
+    color: #c56f6f
 </style>
