008f483cfdf3192146a73701ca051e218d8b732f
macbook <kozlovatana366@gmail.com>
2023-05-27 23:39:06 +0300

    arrow added

---
+++ src/components/common/ScrollUpButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="toUp">
+    <img :src="Arrow" alt="up-button"/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ScrollUpButton"
+}
+</script>
+<script setup>
+import Arrow from "@/assets/arrow-up.png"
+
+const toUp = function () {
+  window.scroll(0, 0)
+}
+</script>
+
+<style scoped lang="sass">
+button
+  right: 50px
+  bottom: 60px
+  border: none
+  position: fixed
+  background-color: transparent
+
+  opacity: 0
+  transition: opacity 0.1s ease-in-out
+</style>
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -6,6 +6,7 @@
         :key="index"
         :painting="painting"
     ></painting-card>
+    <ScrollUpButton :class="{ show : isShown }"/>
   </div>
 </template>
 
@@ -19,14 +20,33 @@ export default {
 import PaintingCard from "@/components/painting/PaintingCard.vue";
 import { paintingsMocks } from "@/mocks/paintings.js";
 import { ref } from "vue";
+import ScrollUpButton from "@/components/common/ScrollUpButton.vue";
 
 const paintings = ref(paintingsMocks);
+const isShown = ref(false);
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 300 ) {
+    showButton()
+  }
+  if (window.scrollY < 300) {
+    isShown.value = false
+  }
+})
+
+const showButton = () => {
+  isShown.value = true
+}
 </script>
 
 <style scoped lang="sass">
 .paint-container
   width: 100%
   display: flex
+  position: relative
   margin: 0 auto 100px
   flex-direction: column
+
+  .show
+    opacity: 100%
 </style>
